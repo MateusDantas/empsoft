37ff931b649c26a8c6a58e325b9a786fc3c35fc5
Andre335 <andre335@gmail.com>
2017-08-14 01:52:28 -0300

    Tela perfil com comentarios e detalhes

--- app/containers/UserPage/index.js
+++ app/containers/UserPage/index.js
@@ -1,23 +1,29 @@
 import React from 'react';
 import classNames from 'classnames/bind';
 import { Row, Col } from 'react-bootstrap';
-
+import StarRatingComponent from 'react-star-rating-component';
+import { Scrollbars } from 'react-custom-scrollbars';
 import styles from './styles.css';
+import UserAvatar from 'react-user-avatar';
 
 const cx = classNames.bind(styles);
 
 export class UserPage extends React.Component { // eslint-disable-line react/prefer-stateless-function
+  props: Object;
   render() {
     return (
       <div className={cx('page-body')}>
         <div className={cx('page-container')}>
           <Row>
-            <Col xs={1} md={2} xsOffset={1}>
+            <Col xs={2} md={3} xsOffset={1}>
               <div className={cx('page-services')}>
               </div>
             </Col>
-            <Col>
-              <Comments />
+            <Col xs={2} md={3} className={cx('description-container')}>
+              <Details {...this.props} />
+            </Col>
+            <Col xs={2} md={3} className={cx('comment-container')}>
+              <Comments {...this.props} />
             </Col>
           </Row>
         </div>
@@ -26,19 +32,76 @@ export class UserPage extends React.Component { // eslint-disable-line react/pre
   }
 }
 
+class Details extends React.Component {
+  props: Object;
+  render() {
+    const currUserName = this.props.users
+        .find((user) => user.get('id').toString() === this.props.params.user_id)
+        .get('name');
+    const currUserRating = this.props.users
+        .find((user) => user.get('id').toString() === this.props.params.user_id)
+        .get('rating');
+    const description = this.props.users
+        .find((user) => user.get('id').toString() === this.props.params.user_id)
+        .get('detailedDescription');
+    return (
+      <ul>
+        <div className={cx('details-box-title')}> Detalhes: </div>
+        <div className={cx('details-box')}>
+          <div className={cx('details-box-name')}>
+            <UserAvatar size="35" name={currUserName} />
+            {currUserName}
+          </div>
+          <div className={cx('details-box-info')}>
+            {description}
+          </div>
+          <StarRatingComponent
+            name="app5"
+            starColor="#ffb400"
+            emptyStarColor="#ffb400"
+            value={currUserRating}
+            editing={false}
+            renderStarIcon={(index, value) => <span className={index <= value ? 'fa fa-star' : 'fa fa-star-o'} />}
+            renderStarIconHalf={() => <span className="fa fa-star-half-full" />}
+          />
+        </div>
+      </ul>
+    );
+  }
+}
+
 class Comments extends React.Component {
   props: Object;
   render() {
     const comments = this.props.users
-        .find((user) => user.get('id') === this.props.params.user_id)
+        .find((user) => user.get('id').toString() === this.props.params.user_id)
         .get('comments')
         .sortBy((comment) => -comment.get('id'))
         .toJS();
     return (
       <ul>
-        { comments.map((comment) => (
-          <li key={comment.id}> {comment.text} </li>
-        ))}
+        <div className={cx('comment-box-title')}> Comentários: </div>
+        <Scrollbars style={{ width: '100%', height: '80vh' }}>
+          { comments.map((comment) => (
+            <div className={cx('comment-box')}>
+              <li key={comment.id}>
+                <div className={cx('comment-box-info')}>
+                  <UserAvatar size="35" name={comment.name} />
+                  <div className={cx('comment-name')}>{comment.name} Diz: {comment.text}</div>
+                  <StarRatingComponent
+                    name="app5"
+                    starColor="#ffb400"
+                    emptyStarColor="#ffb400"
+                    value={comment.rating}
+                    editing={false}
+                    renderStarIcon={(index, value) => <span className={index <= value ? 'fa fa-star' : 'fa fa-star-o'} />}
+                    renderStarIconHalf={() => <span className="fa fa-star-half-full" />}
+                  />
+                </div>
+              </li>
+            </div>
+          ))}
+        </Scrollbars>
       </ul>
     );
   }
