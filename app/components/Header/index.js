19625fb80beec16a0abe872f0bcbf0981bcb22bb
Mateus Dantas <mateuscd10@gmail.com>
2017-08-28 10:39:13 -0300

    Adding notification icon

--- app/components/Header/index.js
+++ app/components/Header/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Badge, Icon, Popconfirm, message } from 'antd';
 import { browserHistory } from 'react-router';
 import classNames from 'classnames/bind';
 
@@ -21,8 +22,15 @@ class Header extends React.Component { // eslint-disable-line
             Contratar <i className={cx('fa', 'fa-sort-desc', 'header-icon')} aria-hidden="true" />
           </span>
         </div>
-        <div className={cx('header-profile')} onClick={() => browserHistory.push('/profile/999')}>
-          <UserAvatar size="48" name="Mateus Dantas" />
+        <div className={cx('header-profile')}>
+          <Popconfirm
+            placement="bottom"
+            title={'Mateus Dantas está a procura de um serviço de pedreiro começando dia 25 de agosto e terminando 25 de setembro por 800 reais. Você deseja aceitar?'}
+            onConfirm={() => message.info('Click on Yes.')} okText="Sim" cancelText="Não"
+          >
+            <Badge dot><Icon type="notification" /></Badge>
+          </Popconfirm>
+          <UserAvatar onClick={() => browserHistory.push('/profile/8')} size="48" name="Mateus Dantas" />
         </div>
       </div>
     );
